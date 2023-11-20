097d27ad27eb240d766904c27c3f6f00ec067275
jacquelineguo <jacquelinexuanguo@gmail.com>
2023-11-19 19:34:00 -0800

    added validation when register

--- WA8_Final_Project_1/WA8_Final_Project_1/Register Screen/RegisterFirebaseManager.swift
+++ WA8_Final_Project_1/WA8_Final_Project_1/Register Screen/RegisterFirebaseManager.swift
@@ -5,6 +5,7 @@
 //  Created by Jacqueline Guo on 11/14/23.
 //
 
+import UIKit
 import Foundation
 import FirebaseAuth
 
@@ -12,17 +13,30 @@ extension RegisterViewController{
     
     func registerNewAccount(){
         showActivityIndicator()
-        if let name = registerView.nameTextField.text,
-           let email = registerView.emailTextField.text,
-           let password = registerView.passwordTextField.text{
-            Auth.auth().createUser(withEmail: email, password: password, completion: {result, error in
-                if error == nil{
-                    self.setNameOfTheUserInFirebaseAuth(name: name)
-                }else{
-                    print(error)
-                }
-            })
+        let name = registerView.nameTextField.text
+        let email = registerView.emailTextField.text
+        let password = registerView.passwordTextField.text
+        if name?.isEmpty == true || email?.isEmpty == true || password?.isEmpty == true {
+            showAlert(with: "Error", message: "All fields must be filled out.")
+            self.hideActivityIndicator()
+            return
         }
+        
+        if !isValidEmail(email) {
+            showAlert(with: "Error", message: "Please provide correct email address.")
+            self.hideActivityIndicator()
+            return
+        }
+        
+        if let name = name, let email = email, let password = password {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: {result, error in
+                        if error == nil{
+                            self.setNameOfTheUserInFirebaseAuth(name: name)
+                        }else{
+                            print(error)
+                        }
+                    })
+                }
     }
 
     func setNameOfTheUserInFirebaseAuth(name: String){
@@ -36,6 +50,18 @@ extension RegisterViewController{
                 print("Error occured: \(String(describing: error))")
             }
         })
+    }
+    
+    private func isValidEmail(_ email: String?) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    private func showAlert(with title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 }
 
